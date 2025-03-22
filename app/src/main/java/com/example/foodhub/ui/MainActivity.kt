5e7d8e1f633995cc23890bf69442a928b4aa940c
Mohammed Gomah <mohammedgomah125@gmail.com>
2025-03-22 23:01:40 +0200

    splash , register beta , login beta

--- app/src/main/java/com/example/foodhub/ui/MainActivity.kt
+++ app/src/main/java/com/example/foodhub/ui/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.foodhub.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
@@ -13,10 +14,10 @@ class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
         navController = navHostFragment.navController
